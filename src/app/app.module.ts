c4722f364e4474bc3a403f056360340b986aa4b1
Rohini-bit <rohini.merikenapalli@gmail.com>
2022-05-23 23:55:43 -0500

    added bootstrap,header and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProductlistsComponent } from './product/productlists/productlists.component';
+import { ProductviewComponent } from './product/productview/productview.component';
+import { UsersComponent } from './users/users.component';
+import { OrdersComponent } from './orders/orders.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ShipmentComponent } from './shipment/shipment.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ProductlistsComponent,
+    ProductviewComponent,
+    UsersComponent,
+    OrdersComponent,
+    PaymentComponent,
+    ShipmentComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product/productlists/productlists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productlists',
+  templateUrl: './productlists.component.html',
+  styleUrls: ['./productlists.component.css']
+})
+export class ProductlistsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
